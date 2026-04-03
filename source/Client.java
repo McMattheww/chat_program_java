154d4b032071d85449dea87a8c8a86f0dbb31bb8
Matthew <mvansloot@gmail.com>
2026-04-02 21:01:52 -0700

    initial version, multithreaded connection between server and clients works

--- source/Client.java
+++ source/Client.java
@@ -1,10 +1,22 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 
-public class Client {
+class Client {
 
-    public static void main (String[] args){
+    public static void main(String[] args)
+    {
+        try (Socket socket = new Socket("localhost", 8888)) {
 
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            byte[] data = "hello world".getBytes(StandardCharsets.UTF_8);
+            out.write(data);
+            out.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- source/Server.java
+++ source/Server.java
@@ -1,10 +1,82 @@
 import java.net.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Server {
 
     public static void main (String[] args){
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(8888);
+            server.setReuseAddress(true);
 
+            while (true){
+                Socket client = server.accept();
+                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
+
+                ClientHandler clientSock = new ClientHandler(client);
+                new Thread(clientSock).start();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (server != null) {
+                try {
+                    server.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket socket)
+        {
+            this.clientSocket = socket;
+        }
+
+        public void run(){
+
+            InputStream in = null;
+            try {
+                in = clientSocket.getInputStream();
+
+                byte[] buffer = new byte[1024];
+                int bytes;
+                while ((bytes = in.read(buffer)) != -1) {
+                    System.out.println(buffer);
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
