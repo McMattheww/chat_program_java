aa0959c660d4c74dafa4842898402ba4fea883ab
Matthew <mvansloot@gmail.com>
2026-04-06 13:51:14 -0700

    added comments

--- source/Client.java
+++ source/Client.java
@@ -8,21 +8,24 @@ class Client {
 
     public static void main(String[] args)
     {
+        //try connecting to server, for now the server is limited to one on localhost
         try (Socket socket = new Socket("localhost", 8888)) {
 
+            //assign the byte input and output streams for the server
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             DataInputStream in = new DataInputStream(socket.getInputStream());
-            byte[] data = "hello world".getBytes(StandardCharsets.UTF_8);
-            out.write(data);
-            out.flush();
+
+
+            //variables to hold the buffer
             byte[] buffer = new byte[2048];
             int bytes;
-            //read bytes to display incoming chat messages
+
+            //read bytes to display incoming chat messages in infinite loop
             while ((bytes = in.read(buffer)) != -1) {
                 System.out.println(Arrays.toString(buffer));
             }
-            //create seperate thread to create chat messages and send to output
-        }
+
+        }//error connecting to server
         catch (IOException e) {
             e.printStackTrace();
         }
--- source/Server.java
+++ source/Server.java
@@ -5,25 +5,34 @@ import java.util.*;
 public class Server {
 
     public static void main (String[] args){
+
+        //start the server on port 8888
         ServerSocket server = null;
         try {
             server = new ServerSocket(8888);
             server.setReuseAddress(true);
 
+            //endless loop while server is running
             while (true){
+                //accept client connection and create socket
                 Socket client = server.accept();
+                //print connected host info
                 System.out.println("New client connected" + client.getInetAddress().getHostAddress());
 
+                //create a new clientHandler object and create new thread for it
                 ClientHandler clientSock = new ClientHandler(client);
                 new Thread(clientSock).start();
             }
         }
+        //error starting the server
         catch (IOException e) {
             e.printStackTrace();
         }
+        //once server closes, never called now because of the previous while true loop
         finally {
             if (server != null) {
                 try {
+                    //close server if it was successfully opened
                     server.close();
                 }
                 catch (IOException e) {
@@ -36,25 +45,30 @@ public class Server {
     private static class ClientHandler implements Runnable {
         private final Socket clientSocket;
 
+        //keep track of the client's socket during construction
         public ClientHandler(Socket socket)
         {
             this.clientSocket = socket;
         }
 
+        //method runs when the new thread is created
         public void run(){
 
             InputStream in = null;
             OutputStream out = null;
             try {
+                //get client input/output byte streams
                 in = clientSocket.getInputStream();
                 out = clientSocket.getOutputStream();
 
+                //create buffer to receive input from client
                 byte[] buffer = new byte[1024];
                 int bytes;
+                //while buffer read from client is not -1 size
                 while ((bytes = in.read(buffer)) != -1) {
+                    //keep reading input and output to the server
                     System.out.println(buffer);
                 }
-                out.write(1000);
             }
             catch (IOException e) {
                 e.printStackTrace();
